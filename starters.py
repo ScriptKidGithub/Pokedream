5816e34023b8a1506d38d2d2748dd750e8bbe0cd
ScriptKid <90466847+ScriptKidGithub@users.noreply.github.com>
2022-01-01 20:31:05 +0530

    Create starters.py

---
+++ starters.py
@@ -0,0 +1,3 @@
+
+STARTERS = [
+"Bulbasaur", "Charmander", "Squirtle", "Chikorita", "Cyndaquil", "Totodile", "Treecko", "Torchic", "Mudkip", "Turtwig", "Chimchar", "Piplup" ,"Snivy", "Tepig", "Oshawott","Chespin", "Fennekin", "Froakie", "Rowlet", "Litten", "Popplio", "Grookey", "Scorbunny", "Sobble", "Rowlet", "Cyndaquil", "Oshawott"]
